e91ab823b1a4c813dbaa46304dc6c36d27a58748
David Shonn <davidlshonn@icloud.com>
2021-02-06 19:13:28 +0000

    Set up server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require("express");
-
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller");
 var app = express();
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-
-
-
-
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log("Connection made...")
-})
+  console.log("Connection made...");
+});
