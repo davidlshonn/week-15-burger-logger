93f5f151ffe181321eb2e2c19600cef801b5e92a
David Shonn <davidlshonn@icloud.com>
2021-02-06 09:02:51 +0000

    orm functions called

---
+++ models/burger.js
@@ -0,0 +1,16 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    selectAll(cb) {
+      orm.selectAll('burgers', (res) => cb(res));
+    },
+    // The variables cols and vals are arrays.
+    insertOne(cols, vals, cb) {
+      orm.insertOne('burgers', cols, vals, (res) => cb(res));
+    },
+    createOne(objColVals, condition, cb) {
+      orm.createOne('burgers', objColVals, condition, (res) => cb(res));
+    }
+  };
+
+module.exports = burger;
