57762f0e546b8090265825a3db856e0da5743a48
David Shonn <davidlshonn@icloud.com>
2021-02-06 07:45:13 +0000

    connection made from node to mysql

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Set up MySQL connection.
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 8080,
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  password: 'Sat-news967',
+  database: 'burger_db',
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
