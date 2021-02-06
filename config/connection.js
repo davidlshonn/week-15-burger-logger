4a793b0a4ba2b2a822a1a751d1977dbcd87e1bb4
David Shonn <davidlshonn@icloud.com>
2021-02-06 19:12:18 +0000

    Set up connection to database

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 8080,
+  port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
   password: 'Sat-news967',
